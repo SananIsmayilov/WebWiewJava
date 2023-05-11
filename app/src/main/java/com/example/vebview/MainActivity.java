50641ce4c62f9b871c27b6e212442fe17261135b
Sanan Ismayilov <ismayilovsenan374@gmail.com>
2023-05-11 11:33:00 +0400

    WebWiew added

---
+++ app/src/main/java/com/example/vebview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.vebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+WebView webView;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.veb);
+        webView.getSettings().getJavaScriptEnabled();
+        //enter link
+        webView.loadUrl("xxxxxx");
+    }
+}
